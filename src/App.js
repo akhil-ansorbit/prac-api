d0183373e080135cb1eb96043b6c1dbb80ec35da
akhil-ansorbit <akhil.gothi@ansorbit.com>
2022-11-24 11:28:19 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Prac from "./Prac";
+
+const App = () => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    const axiosPost = async () => {
+      const respo = await axios("https://jsonplaceholder.typicode.com/users");
+      setUser(respo.data);
+    };
+    axiosPost();
+  }, []);
+
+  return (
+    <div>
+      {/* {console.log(user)} */}
+      {user.map((val, id) => {
+        return <div key={val.id}>{val.email}</div>;
+      })}
+    </div>
+  );
+};
+
+export default App;
